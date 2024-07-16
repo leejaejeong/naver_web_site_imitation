403e6a92229291e524fe796a0d1167f799aab849
leejaejeong <jaejeonglee6@gmail.com>
2024-07-16 17:58:05 +0900

    왼쪽 섹션(배너, 뉴스, 쇼핑컨텐츠) 작업 완료

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+    $(document).ready(function(){
+        $('.news_sec .news_slide > .box01').click(function(){
+            $('.news_sec .news_slide > .box .blind').toggleClass('show');
+            $('.news_sec .news_slide .arrow_box img').toggleClass('down');
+        })
+
+        $('.tab_list > li').click(function(){
+            $('.tab_list > li').removeClass('active');
+            $(this).addClass('active');
+            // $('.news_sec > div').eq($(this).index()).fadeIn(300).siblings('.news_sec > div').hide();
+        }).filter(':eq(0)').click();
+    })
+})
